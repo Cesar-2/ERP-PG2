cd9b8901963b004cccaa2a3fe1fbacbffd67e808
cesaraph <capimient@utp.edu.co>
2021-06-03 13:13:01 -0500

    assessment

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -1,2 +1,3 @@
 from .enterprise import EnterpriseSerializer
 from .employer import EmployerSerializer
+from .assessment import AssessmentSerializer
---
+++ api/serializers/assessment/assessment.py
@@ -0,0 +1,42 @@
+import copy
+
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from ...models.evaluation import Assessment
+
+from ...serializers.employer import EmployerSerializer
+
+
+class AssessmentSerializer(serializers.ModelSerializer):
+    """ Defines assessment serializer behaviour. """
+    employee_evaluated = EmployerSerializer(read_only=True)
+    employee_evaluator = EmployerSerializer(read_only=True)
+
+    class Meta:
+        """ Defines serializer fields that are being used """
+        model = Assessment
+        fields = ["pk", "type", "employee_evaluated", "employee_evaluator",
+                  "qualification", "creation_date"]
+
+    def __str__(self):
+        return (
+            f"{self.pk} - {self.employee_evaluated} {self.employee_evaluator} {self.qualification}")
+
+    def create(self, validated_data):
+        """ Creates an assessment object with its password and relate it with
+            its modules.
+
+        Parameters:
+            validated_data (dict):Contains the data from assessment that is going to
+                                  be created.
+
+        Returns:
+            assessment (Assessment): A fields-full custom django assessment.
+
+        """
+        aux = copy.deepcopy(validated_data)
+        assessment = Assessment.objects.create(**aux)
+        assessment.save()
+
+        return assessment
